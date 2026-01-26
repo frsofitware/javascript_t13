8ad86d1b50f33a345435b1fee808e20fe079aeb3
Sofia Sabrina Silva <sofiasabrinasilva@gmail.com>
2026-01-26 12:14:53 -0300

    Primeiros passos em JavaScript.

---
+++ helloworld/HelloWorld.js
@@ -0,0 +1,8 @@
+const leia = require("readline-sync")
+
+let numero1 = leia.questionInt("Digite o primeiro numero: ")
+let numero2 = leia.questionInt("Digite o segundo nu232mero: ")
+
+soma = numero1 + numero2
+
+console.log(soma)
